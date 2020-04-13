670546515045e2a92f5a2f8296f1fdaa23dee460
asketonim <anton.dubik33@gmail.com>
2020-04-13 21:12:13 +0300

    Fix eslint errors in data and PostModel

--- UI/js/data.js
+++ UI/js/data.js
@@ -1,182 +1,183 @@
-let postsRaw = [
+// eslint-disable-next-line no-unused-vars
+const posts = [
   {
     id: 1,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 2,
     author: {
       id: 2,
       name: 'Andrey Alekseev',
-      username: 'asket'
+      username: 'asket',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['secondpost', 'tilter']
+    hashtags: ['secondpost', 'tilter'],
   },
   {
     id: 3,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 4,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 5,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asket'
+      username: 'asket',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 6,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 7,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asket'
+      username: 'asket',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 8,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 9,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 10,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 11,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 12,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 13,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 14,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
+    hashtags: ['firstpost', 'twitter'],
   },
   {
     id: 15,
     author: {
       id: 1,
       name: 'Anton Dubik',
-      username: 'asketonim'
+      username: 'asketonim',
     },
     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
     createdAt: new Date('2020-03-17T23:00:00'),
     likes: [],
-    hashtags: ['firstpost', 'twitter']
-  }
-]
+    hashtags: ['firstpost', 'twitter'],
+  },
+];
